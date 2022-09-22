d748a17b3fa02491dbb5a69e2dbef2ed78c6674e
Edssaac <edssaac@outlook.com>
2022-09-22 20:21:45 -0300

    implementa classe de anuncio

---
+++ App/Models/Anuncio.php
@@ -0,0 +1,37 @@
+<?php
+
+    namespace App\Models;
+
+    use MF\Model\Model;
+    use PDO;
+
+    class Anuncio extends Model {
+
+        protected $cod_anuncio;
+        protected $cod_usuario;
+		protected $titulo;
+		protected $descricao;
+		protected $status;
+		protected $data_anunciada;
+		protected $valor;
+		protected $desconto;
+		protected $data_desconto;
+		protected $tipo;
+
+        // Cadastrar Anuncio:
+
+        // Atualizar Anuncio:
+
+        // Excluir um anuncio:
+
+        // Obter todos os anuncios de produto:
+
+        // Obter todos os anuncios de monitoria:
+
+        // Obter um anuncio em especifico:
+
+        // Obter os últimos 5 anúncios:
+
+    }
+
+?>
